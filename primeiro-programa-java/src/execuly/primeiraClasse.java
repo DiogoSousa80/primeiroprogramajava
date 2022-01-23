7c1206c6a15058b7abcfab66ad70d5773594aea3
sousa <sousa@host.docker.internal>
2022-01-23 16:23:49 -0300

    8585

--- primeiro-programa-java/src/execuly/primeiraClasse.java
+++ primeiro-programa-java/src/execuly/primeiraClasse.java
@@ -4,7 +4,7 @@ public class primeiraClasse {
 
 	public static void main(String[] args) {
  		System.out.println("Meu primeiro programa em java");
- 		System.out.println("Esse é meu app");
+ 		System.out.println("Esse é meu app8585");
 
 	
 	
