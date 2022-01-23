e53f17cfa94be11a1c157bafd741504f1958625f
sousa <sousa@host.docker.internal>
2022-01-23 15:15:19 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/execuly/primeiraClasse.java
@@ -0,0 +1,12 @@
+package execuly;
+
+public class primeiraClasse {
+
+	public static void main(String[] args) {
+ 		System.out.println("Meu primeiro programa em java");
+ 		System.out.println("Esse é meu app");
+
+	
+	
+	}
+}
